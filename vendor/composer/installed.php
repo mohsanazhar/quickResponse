52492b752ccb89dd18c129f50c35dbd0dc26f525
mohsanazhat <dawlance64@gmail.ccom>
2023-10-19 07:18:16 +0500

    composer optimized

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.2.6',
-        'version' => '10.2.6.0',
-        'reference' => '36047268f130477fa17270e26b1bb5991d781265',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1738ae52e539c2f03b0470671627100ae2d6cac7',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -371,9 +371,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.2.6',
-            'version' => '10.2.6.0',
-            'reference' => '36047268f130477fa17270e26b1bb5991d781265',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1738ae52e539c2f03b0470671627100ae2d6cac7',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
